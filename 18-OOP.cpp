c2e5e248acf9c286257ed7ff50111c8c522809ba
hairpin <60984178+Kushnirve@users.noreply.github.com>
2020-10-03 03:14:20 +0300

    added code

---
+++ 18-OOP.cpp
@@ -0,0 +1,20 @@
+﻿#include "Counter.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+    Counter counter(4,8);
+    counter.plusOne();
+    cout << counter.getData() << endl;
+    counter.setMin(6);
+    counter.plusOne();
+    cout << counter.getData() << endl;
+    counter.setMax(9);
+    cout << counter.getData() << endl;
+    counter.plusOne();
+    counter.plusOne();
+    counter.plusOne();
+    cout << counter.getData() << endl;
+    counter.plusOne();
+    cout << counter.getData() << endl;
+}
